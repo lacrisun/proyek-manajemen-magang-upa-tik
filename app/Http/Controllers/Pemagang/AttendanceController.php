f0da9688c36185a3331c47acc34a5859d3633dcb
nafitarmizi <hanafitarmizi97@gmail.com>
2026-08-06 16:25:17 +0700

    update

--- app/Http/Controllers/Pemagang/AttendanceController.php
+++ app/Http/Controllers/Pemagang/AttendanceController.php
@@ -234,7 +234,7 @@ class AttendanceController extends Controller
     public function record(Request $request, string $type)
     {
         $clientIp = $request->ip();
-        $isIpAllowed = $clientIp === '103.10.169.26';
+        $isIpAllowed = Str::startsWith($clientIp, '103.') || Str::startsWith($clientIp, '172.') || $clientIp === '127.0.0.1';
 
         if (! $isIpAllowed) {
             return back()->with([
